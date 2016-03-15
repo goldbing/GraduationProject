9604a9b2d99106f7c3c3c3c19d0ddc24debe49ec
dmboy <893658963@qq.com>
2016-03-15 13:08:30 +0800

    changes

--- app/src/main/java/com/example/asus/graduationproject/MainActivity.java
+++ app/src/main/java/com/example/asus/graduationproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initView(new View(this));
+    }
+    
+    private void initView(View v) {
+        
     }
 
     @Override
